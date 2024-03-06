1d5a3a98712493e86c0abf5aad645043e7ad1e3f
Blu-kode <161450066+Blu-kode@users.noreply.github.com>
2024-03-06 18:00:20 +0500

    Improve app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const oberser = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => oberser.observe(el));
